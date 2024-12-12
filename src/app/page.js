9d4fa3589025f2fef7eaee21497738fbeaa57230
sabamiso <yabuki1374@gmail.com>
2024-12-12 17:19:47 +0900

    リンク生成周りの可読性向上 cookieへの書き込み実装

--- src/app/page.js
+++ src/app/page.js
@@ -89,33 +89,44 @@ function PlayListCluster(){
   );
 }
 
-function Clip({ name, title, epnum, username, icon, rating, url }) {
-  // Link generation
+
+function Clip({ name, title, epnum, username, icon, rating, url ,starttime, endtime}) {
+  // Link 生成
   let urlLink;
   switch (icon) {
     case "netflix":
       urlLink = "https://www.netflix.com"+url;
       break;
     case "prime":
-      urlLink = "https://www.amazon.co.jp/primevideo"+url;
+      urlLink = "https://www.amazon.co.jp/primevideo"+url;//暫定なので気にしないで
       break;
     default:
       urlLink = null; // Handle unknown cases
   }
+  //クリック時の処理
+  const handleClick = () =>{
+    // cookie関係　starttime endtime拡張機能側への受け渡し
+    document.cookie = `title=${name}; starttime=${starttime}; endtime=${endtime} `;
+    console.log(document.cookie);
+    alert("Cookieに値を書き込みました！");
+    
+    
+    //リンクを開く処理
+    if (urlLink) {
+      window.open(urlLink, "_blank");
+    } else {
+      alert("Invalid link or unknown service");//eroorリンクへの変更
+    }
+
+  }
+
+
 
   return (
     <div className="list-item">
       <p>
         {name} — {title} {epnum} — {username}{" "}
-        <button
-          onClick={() => {
-            if (urlLink) {
-              window.open(urlLink, "_blank");
-            } else {
-              alert("Invalid link or unknown service");
-            }
-          }}
-        >
+        <button onClick={handleClick}>
           {icon}
         </button>{" "}
         {rating} +
@@ -145,7 +156,7 @@ function ClipList(){
           }
           const json = await response.json();
 
-          console.log("取得したデータ:", json); // データ構造を確認
+          //console.log("取得したデータ:", json); // データ構造を確認
           setData(json); // データをステートに保存
         } catch (err) {
           console.error("データ取得エラー:", err);
@@ -170,6 +181,8 @@ function ClipList(){
             url={item.URL || "/browse"}
             username="ユーザー名"
             icon="netflix" // 動的に変更可能
+            starttime={item.StartTime}
+            endtime={item.EndTime}
           />
         ))
       ) : (
